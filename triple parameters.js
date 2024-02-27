41dace0c0f3ed96fb32d2ef6b5ea8aa844ced709
Luca Rossi <161530943+Luca100Rossi@users.noreply.github.com>
2024-02-28 01:03:47 +0200

    Create triple parameters.js

---
+++ triple parameters.js
@@ -0,0 +1,15 @@
+{  "sender" : "",
+  "supportEmail" : "example@example.com",
+  "costPerBlock" : 1
+}
+
+    {
+      "cookbookID": "testCookbookID",
+      "itemID": "11111111"
+    }
+
+          {
+            "Key": "level",
+            "MinValue": "1",
+            "MaxValue": "2"
+          }
